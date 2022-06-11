d2429aaf1e73f66f76808cd4c768f528e52d01b8
jiro <johnjiro1114@gmail.com>
2022-06-11 22:54:55 +0900

    added requiremets.text

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
+
+# import numpy as np
+# import pandas as pd
+# from PIL import Image
 import time
 
 st.title("Stermlit 超入門")
